dbbb27a642561cda0670b44b0eca3c4bea607b32
Kinga Kohari <kinguca@yahoo.com>
2023-01-25 13:17:01 +0100

    SAPUI5 walkthrough started

---
+++ index.js
@@ -0,0 +1,41 @@
+/* sap.ui.define([
+	"sap/m/Button",
+	"sap/m/MessageToast"
+], function (Button, MessageToast, Text) {
+	"use strict";
+
+	alert("UI5 is ready");
+
+	new Button({
+		text: "Ready...",
+		press: function () {
+			MessageToast.show("Hello World!");
+		}
+	}).placeAt("content");
+
+}); */
+
+sap.ui.define([
+	"sap/m/Text"
+
+], function (Text) {
+	"use strict";
+
+	new Text({
+		text: "Hello World"
+	}).placeAt("content");
+
+}); 
+
+/* sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+	"use strict";
+
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+
+}); */
